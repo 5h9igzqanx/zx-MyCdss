0da2d7c11eaccb4a6b80b563b5e0aeea103bdb8d
风雨有阻3960 <zhang_qixuan59@163.com>
2022-12-29 05:45:53 +0800

    0.1.4

--- cmds.py
+++ cmds.py
@@ -1,10 +1,11 @@
 import time,os
+import docs.help as z_hp
 def welcome():
     print("Welcome!")
     print("As you can see, this is a particularly simple console applet.")
     print("You can enter some commands after \">>\" or ask me some questions.")
     print("If you are not familiar with it, you can search \"2109511809\" on QQ to find me, or enter \"Help\" after \">>\" to get help documents.")
-    print("If you want to exit, you can enter \"exit\" or \"quit\" or \"bye\" after \">>\", or press the close button at y")
+    print("If you want to exit, you can enter \"exit\" or \"quit\" or \"bye\" or \"pause\" after \">>\", or press the close button at y")
     input_z()
 def input_z():
     input_zz = input('>>')
@@ -14,12 +15,18 @@ def input_z():
 def judge_z(input_zz):
     if input_zz == "":
         input_z()
-    elif input_zz == "quit" or input_zz == "exit" or input_zz == "bye" or input_zz == "退出":
+    elif input_zz == "quit" or input_zz == "exit" or input_zz == "bye" or input_zz == "pause" or input_zz == "退出":
         if input_zz == "bye":
             print("∟ See you!")
+            time.sleep(0.5)
+        elif input_zz == "pause":
+            print("paused")
+            os.system("pause")
         else:
             print("∟",input_zz, "okay")
-        time.sleep(0.5)
+            time.sleep(0.5)
+    elif input_zz == "Help" or input_zz == "help":
+        z_hp.z_help()
     elif input_zz == "a":
         print("∟ aassxsw114514")
         input_z()
---
+++ docs/help.py
@@ -0,0 +1,8 @@
+def z_help():
+    print("help")
+    print("You can ask some questions here.")
+def Z_help_input():
+    while hp_input == "":
+        hp_input = input("help>>")
+def hp_jduge():
+    pass
