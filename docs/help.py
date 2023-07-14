e47a050155daab9025bf7876f5f6d9a85bf1e26b
5h9igzqanx <zhang_qixuan59@163.com>
2023-07-14 20:39:18 +0800

    master

--- docs/help.py
+++ docs/help.py
@@ -14,16 +14,21 @@ def hp_jduge(hp_input):
         def docs():
             f = open("docs\\help.txt",encoding='utf-8')
             x = f.read()
-            print("∟     The following is the content of the help document.")
+            print("∟   以下是帮助文档的内容。")
+            print()
             print(x)
-            print("∟     The above is the content of the help document.")
+            print()
+            print("∟   以上是帮助文档的内容。")
             f.close()
         docs()
         z_help_input()
     elif hp_input in ["exit","bye","quit","退出"]:
         z_exitcmd.input_z()
-    elif hp_input in ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]:
-        print("∟     These commands are for testing. You can understand them as placeholders.")
-        print("∟     Actually, I haven't figured out what commands to write.")
-        print("∟     lol")
+    #elif hp_input in ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]:
+    #    print("∟   这些命令用于测试。您可以把它们理解为占位符")
+    #    print("∟   事实上，我还没弄清楚该写什么命令")
+    #    print("∟   lol")
+    else:
+        print("∟   额，其实我要说的都在帮助文档里了哈哈……")
+        print("∟   所以这个就没什么用了哈哈哈哈……哈哈……")
     z_help_input()
