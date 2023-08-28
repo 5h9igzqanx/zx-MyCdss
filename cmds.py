fbade631f8af95a96957a5eed1b4a6c85686bd32
5h9igzqanx <zhang_qixuan59@163.com>
2023-08-28 10:16:09 +0800

    master

--- cmds.py
+++ cmds.py
@@ -5,37 +5,40 @@
 
 
 import time,os,sys
-import commands.default as z_sys
+import commands.default as z_dft
 import commands.user_custom as z_user
-import docs.help as z_sys_hp
+import docs.help as z_dft_hp
 def welcome():
     print("欢迎！")
     time.sleep(1)
     print("正如您所看到的，这是一个特别简单的命令行终端")
-    print("*您可以在\">>\"之后输入一些命令以达成你的一些目的。")
-    print("*如果您对它有问题，您可以在\">>\"之后输入\"help\"来获取帮助文档，或者在\"https://5h9igzqanx.github.io/TRDWBST\"联系作者。")
+    print("*您可以在\">>\"之后输入一些命令以达成你的一些目的")
+    print("*如果您对它有问题，您可以在\">>\"之后输入\"help\"来获取帮助文档，或者在\"https://5h9igzqanx.github.io/TRDWBST\"联系作者")
     print("*如果您想退出，您可以在\">>\"之后输入\"exit\"或点击右上角的关闭按钮")
     time.sleep(1)
     input_z()
 def input_z():
     input_zz = input('>>')
-    while input_zz == '':
+    while not (input_zz):
         input_zz = input('>>')
     judge_z(input_zz)
 def judge_z(input_zz):
-    if input_zz == "":
+    if not (input_zz):
         pass
-    elif input_zz in ["exit","quit","退出"]:
-        z_sys.exit()
-    elif input_zz in ["Help","help"]:
-        z_sys_hp.z_help()
+    elif input_zz in ["exit","quit","退出"]:     #内置命令:退出
+        z_dft.exit()
+    elif input_zz in ["Help","help"]:            #内置命令:帮助
+        z_dft_hp.z_help()
+    # ========================================================
     elif input_zz in ["practice","practise"]:
-        z_sys.practice()
+        z_dft.practice()
     elif input_zz in ["test"]:
-        z_sys.test()
+        z_dft.test()
     elif input_zz in ["tool"]:
-        z_sys.tool()
+        z_dft.tool()
+    elif input_zz in ["毁灭地球","核弹","爆炸"]:
+        z_dft.rickroll()
     else:
-        z_sys.no_command(input_zz)
+        z_dft.no_command(input_zz)
     print()
     input_z()
--- docs/help.py
+++ docs/help.py
@@ -1,8 +1,7 @@
 import time,os,webbrowser
-import cmds as z_exitcmd
 def z_help():
-    print("→ 您可以在这里得到帮助。")
-    print("→ 如果要查看帮助文档，请在\"help>>\"之后输入\"docs\"。")
+    print("→ 您可以在这里得到帮助")
+    print("→ 如果要查看帮助文档，请在\"help>>\"之后输入\"docs\"")
     z_help_input()
 def z_help_input():
     hp_input = input(">>help>")  
@@ -10,32 +9,35 @@ def z_help_input():
         hp_input = input(">>help>")
     hp_jduge(hp_input)
 def hp_jduge(hp_input):
-    if hp_input in["docs","help"]:
+    if hp_input in["docs","help","文档"]:
         def docs():
             f = open("docs\\help.txt",encoding='utf-8')
             x = f.read()
-            print("→   以下是帮助文档的内容。")
+            print("→   以下是帮助文档的内容")
             print()
             print(x)
             print()
-            print("→   以上是帮助文档的内容。")
+            print("→   以上是帮助文档的内容")
             f.close()
         docs()
         z_help_input()
     elif hp_input in ["exit","bye","quit","退出"]:
-        z_exitcmd.input_z()
+        print("→   您退出了\"帮助\"，请继续使用")
+        time.sleep(1)
+        return
     elif hp_input in ["about","关于"]:
         print("→   如果你想参阅我们的更多内容，欢迎前往\"https://5h9igzqanx.github.io/TRDWBST\"！")
         webbrowser.open("https://5h9igzqanx.github.io/TRDWBST",new=0,autoraise=True)
     elif hp_input in ["feedback","反馈"]:
-        print("→   如果你想反馈关于\"zx-MyCdss\"的BUG或者更新建议，欢迎前往\"https://github.com/5h9igzqanx/zx-MyCdss/discussions\"或\"https://5h9igzqanx.github.io/TRDWBST-pages/pages/project/0/feedback.htm\"！")
+        print("→   如果你想反馈关于\"zx-MyCdss\"的BUG或者更新建议，欢迎前往\"https://github.com/5h9igzqanx/zx-MyCdss/discussions\"！")
         webbrowser.open("https://github.com/5h9igzqanx/zx-MyCdss/discussions",new=0,autoraise=True)
         webbrowser.open("https://5h9igzqanx.github.io/TRDWBST-pages/pages/project/0/feedback.htm",new=0,autoraise=True)
     #elif hp_input in ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]:
-    #    print("→   这些命令用于测试。您可以把它们理解为占位符")
+    #    print("→   这些命令用于测试您可以把它们理解为占位符")
     #    print("→   事实上，我还没弄清楚该写什么命令")
     #    print("→   lol")
     else:
         print("→   额，其实我要说的都在帮助文档里了哈哈……")
         print("→   所以这个就没什么用了哈哈哈哈……哈哈……")
+    print()
     z_help_input()
